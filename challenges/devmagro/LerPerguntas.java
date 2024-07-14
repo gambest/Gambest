51379afd4cfca91e8ab9f28f35691a52f3142fc9
gambest <142860019+gambest@users.noreply.github.com>
2024-07-14 16:08:21 -0300

    Step 1 commit
    There is the class that solves the step 1 and this class has methods to count lines, read lines and save all question in a String array.

---
+++ challenges/devmagro/LerPerguntas.java
@@ -0,0 +1,53 @@
+package Desafio1;
+
+import java.io.*;
+
+public class LerPerguntas {
+    public static void main(String[] args) {
+        File arquivo = new File("Desafio1/Perguntas.txt");
+        FileReader pergunta;
+
+        try {
+            pergunta = new FileReader(arquivo);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        BufferedReader buffPergunta = new BufferedReader(pergunta);
+        //int numeroDeLinhas = contarLinhas(buffPergunta);
+        String[] perguntas = salvarPerguntas(buffPergunta);
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(perguntas[i]);
+        }
+    }
+
+    public static String lerLinhas(BufferedReader buff) {
+        try {
+            return buff.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static int contarLinhas(BufferedReader buff) {
+        int linesNumber = 0;
+        while (true) {
+            String linhaLida = lerLinhas(buff);
+            if (linhaLida != null) {
+                linesNumber++;
+            } else {
+                break;
+            }
+        }
+        return linesNumber;
+    }
+
+    public static String[] salvarPerguntas(BufferedReader buff) {
+        String[] perguntas = new String[4];
+        for (int i = 0; i < 4; i++) {
+            perguntas[i] = lerLinhas(buff);
+        }
+        return perguntas;
+    }
+}
